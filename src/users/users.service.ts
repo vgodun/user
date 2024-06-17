2df28beb1da501033a6d16d8fcdf36001fe4e9db
vgodun <vgodunsoftware@gmail.com>
2024-06-17 16:57:15 +0300

    add roles guard

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
@@ -6,21 +6,25 @@ import {RolesService} from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User) private userRepository: typeof User,private roleService:RolesService) {}
-    async createUser(dto:CreateUserDto) {
-        const user= await this.userRepository.create(dto);
-        const role = await this.roleService.getRoleByValue('User')
+
+    constructor(@InjectModel(User) private userRepository: typeof User,
+                private roleService: RolesService) {}
+
+    async createUser(dto: CreateUserDto) {
+        const user = await this.userRepository.create(dto);
+        const role = await this.roleService.getRoleByValue("ADMIN")
         await user.$set('roles', [role.id])
         user.roles = [role]
         return user;
     }
+
     async getAllUsers() {
-        const users = await this.userRepository.findAll({include:{all:true}});
+        const users = await this.userRepository.findAll({include: {all: true}});
         return users;
     }
 
-    async getUserByEmail(email:string) {
-        const user = await this.userRepository.findOne({where:{email},include:{all:true}});
+    async getUserByEmail(email: string) {
+        const user = await this.userRepository.findOne({where: {email}, include: {all: true}})
         return user;
     }
 }
